025b2577545953e18973c70c130365a2fcc8c029
GNRoces <132887624+Supernaut8@users.noreply.github.com>
2023-11-29 05:25:59 -0300

    Update doctorCardExtended.js
    cambio en las siglas que desaparecian al achicar el display

--- src/components/doctorCardExtended.js
+++ src/components/doctorCardExtended.js
@@ -43,12 +43,7 @@ function DoctorCardExtended() {
                                     <LocalHospitalIcon sx={{ color: 'red' }} />
                                     <Typography
                                         variant="h6"
-                                        noWrap
-                                        component="a"
-                                        href="/"
                                         sx={{
-                                            mr: 2,
-                                            display: { xs: 'none', md: 'flex' },
                                             fontFamily: 'monospace',
                                             fontWeight: 700,
                                             letterSpacing: '.3rem',
